5ad639b4ffccff327cfaf4576ddfbfd5170df892
webdev-bfunk <webdev.bfunk@gmail.com>
2024-03-10 13:44:25 -0400

    added components and more files to directory

---
+++ app/data/navlinks.js
@@ -0,0 +1,23 @@
+
+export const navLinks = [
+    {
+        id: 1,
+        title: "Home",
+        path: "/",
+    },
+    {
+        id: 2,
+        title: "Home",
+        path: "/",
+    },
+    {
+        id: 3,
+        title: "Home",
+        path: "/",
+    },
+    {
+        id: 4,
+        title: "Home",
+        path: "/",
+    },
+]
--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 
 export default function Home() {
   return (
-    <main className="py-24 ">
+    <main className="py-24 w-full h-screen bg-gray-200">
       <div className="container bg-gray-200">
         <h1 className="text-3xl font-bold">
           Next JS Starter Boilerplate
         </h1>
       </div>
-
     </main>
   );
 }
